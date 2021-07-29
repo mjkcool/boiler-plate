fef6b43cb1a7f21995f640d62c7ce2a208dc7e45
s2019w05 <s2019w05@e-mirim.hs.kr>
2021-07-29 23:58:02 +0900

    save

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const config = require('./config/hide.json')
 
-mongodb+srv://mjkcool:<password>@mysite.u6oi4.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+const mongoose = require('mongoose')
+mongoose.connect(`mongodb+srv://mjkcool:${config.user_password}@mysite.u6oi4.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`, {
+    useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
+}).then(() => console.log("MongoDB Connected"))
+.catch(err => console.log(err))
+
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
